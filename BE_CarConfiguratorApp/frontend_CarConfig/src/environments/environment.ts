91131e05daee71f3267ab91e7cb41d5cf1c5b38c
dHaEr <dr.hassan.errami@gmail.com>
2025-07-26 11:32:29 +0200

    2

--- BE_CarConfiguratorApp/frontend_CarConfig/src/environments/environment.ts
+++ BE_CarConfiguratorApp/frontend_CarConfig/src/environments/environment.ts
@@ -1,10 +1,9 @@
 //const serverUrl = 'http://ec2-51-21-129-236.eu-north-1.compute.amazonaws.com';
 
-//onst apiUrl='http://localhost:9000' // development
-//const keycloakUrl= 'http://localhost:8080' // development
-
 const apiUrl='http://gateway-service:9000' //deployment
-const keycloakUrl= 'http://keycloak:8080' // deployment
+//const apiUrl='http://localhost:9000' //deployment
+//const keycloakUrl= 'http://keycloak:8080' // deployment
+const keycloakUrl= 'http://localhost:8080' // deployment
 
 export const environment = {
   production: true,
---
+++ BE_CarConfiguratorApp/frontend_CarConfig/src/environments/evironment.dev.ts
@@ -0,0 +1,12 @@
+//const serverUrl = 'http://ec2-51-21-129-236.eu-north-1.compute.amazonaws.com';
+
+const apiUrl='http://localhost:9000' // development
+const keycloakUrl= 'http://localhost:8080' // development
+
+
+export const environment = {
+  production: true,
+  apiUrl: apiUrl,
+  keycloakUrl: keycloakUrl
+};
+
